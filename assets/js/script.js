45980a353a4d3baa9a1440f4169881565e666b24
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2022-10-30 10:56:29 -0700

    Deletes commented out unused code and moved comments around in js, also cleans up code with prettier to get rid of extra space

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,60 +1,47 @@
 // Important DOM elements
-var dateDisplayEl = $('#currentDay')
-
-
-
+var dateDisplayEl = $("#currentDay");
 
 // displays the date in header
 function displayDate() {
-    var rightNow = moment().format('dddd, MMM Do, YYYY');
-    dateDisplayEl.text(rightNow);
-    console.log("what day is it?")
-  }
+  var rightNow = moment().format("dddd, MMM Do, YYYY");
+  dateDisplayEl.text(rightNow);
+  console.log("what day is it?");
+}
 
 displayDate();
 
-
-// function to check time (hour of the day) and 
+// function to check time (hour of the day) and
 var exactHour = moment().hours();
-
-// function timeCheck() {   
-
-// }
-
 
 // how to get hours for each time block
 // id class name or data
-
+// function to change classes of the time blocks
 var rowTime = $(".time-block");
 console.log(rowTime);
 
-rowTime.each(function() {
-    var timeBlockHour = parseInt($(this).attr("id"))
+rowTime.each(function () {
+  var timeBlockHour = parseInt($(this).attr("id"));
 
-if (timeBlockHour < exactHour) {
+  if (timeBlockHour < exactHour) {
     $(this).addClass("past");
-} else if (timeBlockHour === exactHour) {
+  } else if (timeBlockHour === exactHour) {
     $(this).addClass("present");
-} else {
+  } else {
     $(this).addClass("future");
-}
-})
-
-// function to change classes of the time blocks?
-
-
-
+  }
+});
 
 // event listener for the save button
 // call some function, get value from text area, specific area of text area or Element. parent and children, siblings. previous activity. to traverse. suggested by TA
+//local storage
+// need to call local storage twice, display data first, other to save data
+$(".saveBtn").click(function () {
+  console.log("click works");
 
-$(".saveBtn").click(function() {
-    console.log("click works");
+  var time = $(this).parent().attr("id");
+  var textDescription = $(this).siblings(".description").val();
 
-    var time = $(this).parent().attr("id");
-    var textDescription = $(this).siblings(".description").val();
-
-    localStorage.setItem(time, textDescription);
+  localStorage.setItem(time, textDescription);
 });
 
 $("#9am .description").val(localStorage.getItem("9am"));
@@ -66,8 +53,3 @@ $("#14pm .description").val(localStorage.getItem("14pm"));
 $("#15pm .description").val(localStorage.getItem("15pm"));
 $("#16pm .description").val(localStorage.getItem("16pm"));
 $("#17pm .description").val(localStorage.getItem("17pm"));
-
-
-//local storage 
-// need to call local storage twice, display data first, other to save data
-
